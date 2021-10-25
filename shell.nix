186b1ed7d708ffbf5e5a63b85fd497b277f6a3fd
Robin Hack <hack.robin@gmail.com>
2021-10-25 18:50:11 +0200

    shell.nix: rewrite to not use nix-env for perl installation.
    Also reformat/retab source code.

--- shell.nix
+++ shell.nix
@@ -1,15 +1,19 @@
-with import <nixpkgs> {}; {
-  qpidEnv = stdenvNoCC.mkDerivation {
-    name = "old-kernel-dev-environment";
-    buildInputs = [
-        gcc49
-	ncurses5
-	syslinux
-    ];
-   shellHook =
-   ''
-   nix-env -i perl-5.20.3 -f https://github.com/NixOS/nixpkgs/archive/aeaa79dc82980869a88a5955ea3cd3e1944b7d80.tar.gz
-   '';
-  };
- 
+with import <nixpkgs> {};
+
+let oldPkgs =
+    import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/aeaa79dc82980869a88a5955ea3cd3e1944b7d80.tar.gz") {};
+
+    oldPerlPkg = oldPkgs.perl520;
+in
+{
+    oldKernelEnv = stdenvNoCC.mkDerivation
+    {
+        name = "old-kernel-dev-environment";
+        buildInputs = [
+            gcc49
+            ncurses5
+            syslinux
+            oldPerlPkg
+        ];
+    };
 }
