9b3387868ae8df5385adaa25af18097cc75e0c05
Robin Hack <hack.robin@gmail.com>
2021-11-08 18:48:54 +0100

    Add basic userspace support.

--- src/test/run.sh
+++ src/test/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #DEBUG="-s -S"
-MEM="7802k"
+MEM="7808k"
 
-qemu-system-i386 -M microvm $DEBUG -cpu 486 -m "$MEM" -kernel arch/i386/boot/bzImage -nographic -append "console=ttyS0" -serial mon:stdio
+qemu-system-i386 -M microvm $DEBUG -cpu 486 -m "$MEM" -initrd usr/initramfs_data.cpio.gz  -kernel arch/i386/boot/bzImage -nographic -append "console=ttyS0" -serial mon:stdio
 
