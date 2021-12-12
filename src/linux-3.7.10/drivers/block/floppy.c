9ce80f957ec0292cf53dfee999b902eeebffb926
Robin Hack <hack.robin@gmail.com>
2021-12-12 14:09:32 +0100

    kernel: re-apply patches for floppy driver from 5.17.
    From 34ccdc7d8bd4c018e1aa72a5f2ac522f130238c9 Mon Sep 17 00:00:00 2001
    From: Tasos Sahanidis <tasos@tasossah.com>
    Date: Fri, 3 Sep 2021 09:47:58 +0300
    Subject: [PATCH] floppy: Fix hang in watchdog when disk is ejected
    
    When the watchdog detects a disk change, it calls cancel_activity(),
    which in turn tries to cancel the fd_timer delayed work.
    
    In the above scenario, fd_timer_fn is set to fd_watchdog(), meaning
    it is trying to cancel its own work.
    This results in a hang as cancel_delayed_work_sync() is waiting for the
    watchdog (itself) to return, which never happens.
    
    This can be reproduced relatively consistently by attempting to read a
    broken floppy, and ejecting it while IO is being attempted and retried.
    
    To resolve this, this patch calls cancel_delayed_work() instead, which
    cancels the work without waiting for the watchdog to return and finish.
    
    Before this regression was introduced, the code in this section used
    del_timer(), and not del_timer_sync() to delete the watchdog timer.
    
    Link: https://lore.kernel.org/r/399e486c-6540-db27-76aa-7a271b061f76@tasossah.com
    Fixes: 070ad7e793dc ("floppy: convert to delayed work and single-thread wq")
    Signed-off-by: Tasos Sahanidis <tasos@tasossah.com>
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- src/linux-3.7.10/drivers/block/floppy.c
+++ src/linux-3.7.10/drivers/block/floppy.c
@@ -976,7 +976,7 @@ static DECLARE_DELAYED_WORK(fd_timer, NULL);
 static void cancel_activity(void)
 {
 	do_floppy = NULL;
-	cancel_delayed_work_sync(&fd_timer);
+	cancel_delayed_work(&fd_timer);
 	cancel_work_sync(&floppy_work);
 }
 
