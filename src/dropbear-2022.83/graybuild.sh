744e7356aeb9da02384cec56b55c215e346ac60d
Robin Hack <hack.robin@gmail.com>
2022-12-05 10:36:16 +0100

    dropbear: FIX: build on fedora 37 with latest nix updates.

--- src/dropbear-2022.83/graybuild.sh
+++ src/dropbear-2022.83/graybuild.sh
@@ -9,6 +9,8 @@ autoheader
 
 ./configure --enable-static --enable-bundled-libtom --disable-syslog --disable-harden --disable-zlib --disable-shadow --disable-utmp --disable-utmpx --disable-wtmpx --disable-loginfunc --prefix="$(realpath $PWD/../gray386/_install/)"
 
+echo "#define DROPBEAR_SVR_PASSWORD_AUTH 0" > localoptions.h
+
 make -j"$GR_CPUS"
 
 strip dbclient
